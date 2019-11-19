1b81bcc6facd6ec5731a2fc888f080dd6e62decd
Keishin CHOU <keishin.chou@gmail.com>
2019-11-19 22:03:33 +0900

    Implemented Face-ID recgnition.

--- KeyChain/ViewController.swift
+++ KeyChain/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Keishin CHOU. All rights reserved.
 //
 
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,7 +16,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var secret: UITextView!
     
     @IBAction func authenticateTapped(_ sender: UIButton) {
-        unlockSecretMessage()
+//        unlockSecretMessage()
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { (success, authenticationError) in
+                
+                DispatchQueue.main.async {
+                    if success {
+                        self.unlockSecretMessage()
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
     }
     
     override func viewDidLoad() {
